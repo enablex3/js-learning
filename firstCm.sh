860a5c66d3ccdee4e2d80d9b0ac9117662d73eb7
enablex3 <ereyes@citadel.edu>
2024-01-28 19:50:27 -0500

    remove script

--- firstCm.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-echo "# js-learning" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/enablex3/js-learning
-git push -u origin main
